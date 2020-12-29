558fffc773807c71f728bba4852585bf9c776821
Patrick Reynolds <patrick@piki.org>
2020-12-29 17:31:19 -0500

    Add a big block comment

--- kasa.c
+++ kasa.c
@@ -1,3 +1,27 @@
+/* Raw TP-Link (Kasa) command sender/receiver
+ * Each invocation sends a single command and receives a single packet in response.
+ *
+ * This program is a minimal substitute for the tplink-lightbulb Node package.
+ * Because it's C, it's about 40x faster than the Node package.  On a Raspberry
+ * Pi, that matters.
+ *
+ * Building:
+ *   make
+ *
+ * Usage:
+ *   ./kasa <ip-address> <json-blob>
+ *
+ * There's a good list of JSON blobs to try here:
+ *   https://github.com/softScheck/tplink-smartplug/blob/master/tplink-smarthome-commands.txt
+ * Especially:
+ *   - get bulb info: ./kasa <ip> '{"system":{"get_sysinfo":null}}'
+ *   - turn bulb on:  ./kasa <ip> '{"system":{"set_relay_state":{"state":1}}}'
+ *   - turn bulb off: ./kasa <ip> '{"system":{"set_relay_state":{"state":0}}}'
+ *
+ * Written by Patrick Reynolds <dukepiki@gmail.com>
+ * Released into the public domain, or Creative Commons CC0, your choice.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
